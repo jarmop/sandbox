ddec123fab209d5ad5be6d9c0e13b84f3a3ffd70
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2019-11-23 23:33:31 +0200

    Fix syntax highlight. Run code with button.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import Editor from 'react-simple-code-editor';
-import { highlight, languages } from 'prismjs/components/prism-core';
+import Prism from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism.css';
 import './App.css';
 
 let runCode = (code) => {
@@ -24,19 +25,28 @@ let runCode = (code) => {
 //console.log(window.document.querySelector('#output'));
 };
 
+const initialCode = `for (let i = 0; i<5; i++) {
+    console.log('a');
+}`;
+
 function App() {
-  const [code, setCode] = useState("console.log('werg');");
-  useEffect(() => runCode(code));
+  const [code, setCode] = useState(initialCode);
 
   return (
       <div className="App">
         <Editor
           value={code}
-          highlight={code => highlight(code, languages.js)}
           onValueChange={setCode}
+          highlight={code => Prism.highlight(code, Prism.languages.js)}
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 12,
+          }}
         />
-        <div id="output">fgb</div>
-        <iframe style={{display: 'none'}}></iframe>
+        <button onClick={() => runCode(code)}>Run code</button>
+        <div id="output"></div>
+        <iframe title="output" style={{display: 'none'}}></iframe>
       </div>
   );
 }
