cbcad78aea1bb22849998d721547b2ec3d81deb0
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2019-11-24 14:30:30 +0200

    Updates

---
+++ precache-manifest.c7bb0a21c9df4dd2fc33dba4beea5b80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db52e94d140d46bfa02d38fe7db067da",
+    "url": "/sandbox/index.html"
+  },
+  {
+    "revision": "da2fff37bb15a2cf99db",
+    "url": "/sandbox/static/css/2.efbfe3cd.chunk.css"
+  },
+  {
+    "revision": "1938b05b76f811d3e001",
+    "url": "/sandbox/static/css/main.33395bd9.chunk.css"
+  },
+  {
+    "revision": "da2fff37bb15a2cf99db",
+    "url": "/sandbox/static/js/2.52b1fab5.chunk.js"
+  },
+  {
+    "revision": "1938b05b76f811d3e001",
+    "url": "/sandbox/static/js/main.2b707d8a.chunk.js"
+  },
+  {
+    "revision": "fe5879c049cda525f502",
+    "url": "/sandbox/static/js/runtime-main.2b26c0a8.js"
+  }
+]);
